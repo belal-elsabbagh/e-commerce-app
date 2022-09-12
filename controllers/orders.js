3d8467817b7ffc1db3901cb9f6975f2a9dab708d
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-09-12 10:42:20 +0200

    Fixed orders controller

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,21 +1,20 @@
-const { validate } = require('../validation')
+const {validate} = require('../validation')
 const authorize = require('../auth')
-const {orderSchema} = require('../validation').validationSchemas
-const { getOrders, addOrder, deleteOwnOrder } = require('../services').orderServices;
-const { AUTHORIZATION_RESOURCE_NAMES: resource } = require('../config/constants');
+const {validationSchemas: {orderSchema}} = require('../validation')
+const {orderServices} = require('../services');
+const {AUTHORIZATION_RESOURCE_NAMES: resource} = require('../config/constants');
 const mongoose = require("mongoose");
 /**
  * The users controller
- * @param {Express} app 
+ * @param {Express} app
  */
 module.exports = (app) => {
 
     app.get('/orders', async (req, res, next) => {
         try {
             authorize(req.tokenData.user.role, 'read:any', resource.order)
-            res.status(200).json(await getOrders(req.query))
-        }
-        catch (err) {
+            res.status(200).json(await orderServices.get(req.query))
+        } catch (err) {
             next(err)
         }
     });
@@ -23,9 +22,8 @@ module.exports = (app) => {
     app.get('/users/:userId/orders', async (req, res, next) => {
         try {
             authorize(req.tokenData.user.role, 'read:own', resource.order, req.params.userId)
-            res.status(200).json(await getOrders({userId: new mongoose.Types.ObjectId(req.params.userId)}))
-        }
-        catch (err) {
+            res.status(200).json(await orderServices.get({userId: new mongoose.Types.ObjectId(req.params.userId)}))
+        } catch (err) {
             next(err)
         }
     });
@@ -39,9 +37,8 @@ module.exports = (app) => {
                 shippingAddress: req.body.shippingAddress,
             }
             const newOrder = await validate(orderSchema, parsedOrderData)
-            res.status(201).json(await addOrder(newOrder));
-        }
-        catch (err) {
+            res.status(201).json(await orderServices.add(newOrder));
+        } catch (err) {
             next(err)
         }
     });
@@ -49,9 +46,8 @@ module.exports = (app) => {
     app.delete('/users/:userId/orders/:id', async (req, res, next) => {
         try {
             authorize(req.tokenData.user.role, 'delete:own', resource.order, req.params.userId)
-            res.status(200).json(await deleteOwnOrder(req.params.id, req.tokenData.user.id));
-        }
-        catch (err) {
+            res.status(200).json(await orderServices.deleteOwnOrder(req.params.id, req.tokenData.user.id));
+        } catch (err) {
             next(err)
         }
     })
