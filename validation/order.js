39c1e85c18dadafd5d4ab74ec1ac9be80c812154
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-09-28 12:55:27 +0200

    Added default quantity

--- validation/order.js
+++ validation/order.js
@@ -3,7 +3,7 @@ module.exports = new Joi.object({
     userId: Joi.string().required(),
     products: Joi.array().items({
         product: Joi.string().required(),
-        quantity: Joi.number().min(1).integer().required()
+        quantity: Joi.number().min(1).integer().default(1)
     }).required(),
     shippingAddress: Joi.string().required(),
 })
