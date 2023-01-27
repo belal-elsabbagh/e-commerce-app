12d81c206091a8b8a6c0bfb9738191f6475977e5
Belal Elsabbagh <belsabbagh@gmail.com>
2023-01-27 17:33:47 +0200

    Update auth.js

--- services/auth.js
+++ services/auth.js
@@ -1,27 +1,28 @@
-const {userService} = require('./user')
-const {NotAuthenticatedError} = require('../errors')
+const { userService } = require('./user')
+const { NotAuthenticatedError, InternalServerError } = require('../errors')
 
 class AuthServices {
-    /**
-     *
-     * @param {Object} userObject The user object to add to the database
-     * @throws {NotAuthenticatedError} if the query doesn't find a user with these credentials
-     * @returns {Object} The data of the user that was created
-     */
-    async _runLoginQuery(userObject) {
-      try {
-        let queryResult = await userService.get(userObject)
-        return queryResult;
-      } catch(err) {
-        if (err.code === 404) throw new NotAuthenticatedError('Incorrect Credentials to login');
-      }
+  /**
+   *
+   * @param {Object} userObject The user object to add to the database
+   * @throws {NotAuthenticatedError} if the query doesn't find a user with these credentials
+   * @returns {Object} The data of the user that was created
+   */
+  async _runLoginQuery(userObject) {
+    try {
+      return await userService.get(userObject);
+    } catch (err) {
+      if (err.code === 404) throw new NotAuthenticatedError('Incorrect Credentials to login');
+      throw new InternalServerError("An unexpected error has occured while logging in.")
     }
+  }
 
-    async login(userCredentials) {
-        const user = await this._runLoginQuery(userCredentials);
-        const token = this.model.generateToken(user)
-        return {token, user}
+  async login(userCredentials) {
+    return {
+      token: this.model.generateToken(user),
+      user: await this._runLoginQuery(userCredentials)
     }
+  }
 }
 
 module.exports = new AuthServices()
