e020b30b5b849749015bd842b4b4621359b214f5
Belal Elsabbagh <belsabbagh@gmail.com>
2023-01-27 17:35:01 +0200

    Update user.js

--- services/user.js
+++ services/user.js
@@ -1,7 +1,7 @@
-const {userModel} = require('../models')
-const {NotAuthenticatedError} = require('../errors')
+const { userModel } = require('../models')
+const { NotAuthenticatedError } = require('../errors')
 const BaseService = require('../models/BaseService');
-const {database} = require('../config');
+const { database } = require('../config');
 
 class UserServices extends BaseService {
     constructor() {
@@ -20,24 +20,6 @@ class UserServices extends BaseService {
             }
         ]
         return this.model.aggregate(getUsersWithOrdersQuery)
-    }
-
-    /**
-     *
-     * @param {Object} userObject The user object to add to the database
-     * @throws {NotAuthenticatedError} if the query doesn't find a user with these credentials
-     * @returns {Object} The data of the user that was created
-     */
-    async runLoginQuery(userObject) {
-        let queryResult = await this.model.findOne(userObject)
-        if (queryResult === null) throw new NotAuthenticatedError('Incorrect Credentials to login');
-        return queryResult
-    }
-
-    async login(userCredentials) {
-        const user = await this.runLoginQuery(userCredentials);
-        const token = this.model.generateToken(user)
-        return {token, user}
     }
 }
 
